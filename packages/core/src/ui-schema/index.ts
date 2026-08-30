4a7c2fd22a494459b9d2011e2ae235dfae57aaef
khmilevoi <khmilevoi@gmail.com>
2026-08-30 05:35:53 +0200

    feat(core): publish the editor schema derivations on the package surface

---
+++ packages/core/src/ui-schema/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Editor control descriptors: the browser-safe descriptor types, the `unrepresentable` callback that
+ * resolves registered asset fields, and the two derivations. Re-exported wholesale from the package
+ * barrel.
+ */
+
+export * from './derive.js'
+export * from './descriptor.js'
+export * from './unrepresentable.js'
